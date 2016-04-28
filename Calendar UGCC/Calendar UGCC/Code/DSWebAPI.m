f73364bc2f304fc5df64aa89193b1889b5278867
mgontar <max.gontar@gmail.com>
2016-04-28 17:01:24 +0300

    Data and code update - up to new web service

--- Calendar UGCC/Calendar UGCC/Code/DSWebAPI.m
+++ Calendar UGCC/Calendar UGCC/Code/DSWebAPI.m
@@ -12,12 +12,20 @@
 
 
 + (void)getYear:(NSInteger) year month:(NSInteger) month day:(NSInteger) day withCompletionBlock:(void (^)(DSDay* day, NSError *error))complete{
-    NSString* url = [NSString stringWithFormat:@"http://10.10.0.81:8181/%d/%02d/%02d", (int)year, (int)month, (int)day];
+    NSString* url = [NSString stringWithFormat:@"http://derkach.xyz:8080/dsCalendar/DSCalendarDataService/%d/%02d/%02d", (int)year, (int)month, (int)day];
     AFHTTPSessionManager* manager = [AFHTTPSessionManager manager];
     [manager GET:url parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
         NSDictionary * jsonDictionary = (NSDictionary *)responseObject;
-        DSDay* result = [DSDay fromDictionary:jsonDictionary];
-        complete(result, nil);
+        
+        if(jsonDictionary[@"dsDayFull"])
+        {
+            DSDay* result = [DSDay fromDictionary:jsonDictionary[@"dsDayFull"]];
+            complete(result, nil);
+        }
+        else
+        {
+            complete(nil, [NSError errorWithDomain:@"" code:0 userInfo:@{@"":@""}]);
+        }
     } failure:^(NSURLSessionTask *operation, NSError *error) {
         NSLog(@"Error: %@", error);
         complete(nil, error);
